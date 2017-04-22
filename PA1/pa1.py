971c8d8df96a72abc744583d38c43e5f4dcb3f3a
Qing Dai <qingdai54@gmail.com>
2017-04-21 22:42:56 -0700

    p_value, p_value_significant, chi_square, output_file

--- PA1/pa1.py
+++ PA1/pa1.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from scipy.stats import norm, chisquare
 from math import sqrt
 
 
@@ -13,18 +14,16 @@ def compute_frequency(df, n):
         # print('b[1]', b[1])
         # print('b[2]', b[2])
         p = (df[1] + 2 * df[2]) / n
-
-
-    elif 1 in b.index:
+    elif 1 in df.index:
         # print('b[1]', b[1])
         p = (df[1]) / n
 
-    elif 2 in b.index:
+    elif 2 in df.index:
         # print('b[2]', b[2])
         p = ( 2 * df[2]) / n
     else:
         # print('none')
-        p = 0
+        p = 1
 
     return p
 
@@ -33,22 +32,21 @@ def compute_frequency(df, n):
 def compute_association_statistics(n, case, control):
 
     p_pos = compute_frequency(case, n)
-    print('p_pos = ', p_pos)
+    # print('p_pos = ', p_pos)
     p_neg = compute_frequency(control, n)
-    print('p_neg = ', p_neg)
+    # print('p_neg = ', p_neg)
 
-    if (p_pos !=1) and (p_neg != 1):
+    if (p_pos != 1) and (p_neg != 1):
         p = (p_pos + p_neg) / 2
-        s = (p_pos - p_neg) / (sqrt(2 / n) * sqrt(p * (1 - p)))
+        s = ((p_pos - p_neg) / (sqrt(2 / n) * sqrt(p * (1 - p) ) ) )
 
     else :
         s = 0
 
+
     return s
 
 
-N = 10
-alpha = 0.05
 
 
 
@@ -58,23 +56,96 @@ input_file = "SNP_status.txt"
 # usecols=list(range(0, 100000)): Return a subset of the columns, specified by column number [0, 1, 2, ... 100000]
 # low_memory=False: use low memory
 # a = read_csv(input_file, delimiter=' ', index_col=0, usecols=list(range(0, 100002)), low_memory=False,  nrows=10)
-df = pd.read_csv(input_file, delimiter=' ', index_col=0, usecols=list(range(0, 100002)), low_memory=False, nrows=2000)
+df = pd.read_csv(input_file, delimiter=' ', index_col=0, usecols=list(range(0, 100002)), low_memory=False)
 
+alpha = 0.05
 N = 2000
+alpha_s = alpha/100000
+threshold = -norm.ppf(alpha_s / 2)
 
 # split the dataframne by Case and Control
 case = df[df['Status'] == 'Case']
 control = df[df['Status'] == 'Control']
 
-print(case)
-print(control)
+# print(case)
+# print(control)
+p_values = []
+p_values_significant = []
+chi_square = []
+for i in range(0,100000):
+    # print(i)
+    if i < 10:
 
-a = case['SNP00000'].value_counts()
-b = control['SNP00000'].value_counts()
-print(a)
-print(b)
+        SNPname = 'SNP0000{}'.format(i)
+    elif i < 100:
+        SNPname = 'SNP000{}'.format(i)
+    elif i < 1000:
+        SNPname = 'SNP00{}'.format(i)
+    elif i < 10000:
+        SNPname = 'SNP0{}'.format(i)
+    else:
+        if i == 10000:
+            print(10000)
+        elif i == 20000:
+            print(20000)
+        elif i == 30000:
+            print(30000)
+        elif i == 40000:
+            print(40000)
+        elif i == 50000:
+            print(50000)
+        elif i == 60000:
+            print(60000)
+        elif i == 70000:
+            print(70000)
+        elif i == 80000:
+            print(80000)
+        elif i == 90000:
+            print(90000)
+        SNPname = 'SNP{}'.format(i)
 
 
-s = compute_association_statistics(N, a, b )
-print (s)
+
+
+
+    a = case[SNPname].value_counts()
+    b = control[SNPname].value_counts()
+    # print(a)
+    # print(b)
+
+
+    s = compute_association_statistics(N, a, b)
+
+    # print (s, '\n\n\n')
+    p_values.append([SNPname, s])
+    c2 = s*s
+    chi_square.append(c2)
+    if (s > threshold):
+        p_values_significant.append([SNPname, s])
+
+
+chi_square.sort()
+median = (chi_square[49999] + chi_square[50000]) / 2
+
+for i in range(0,100000):
+    chi_square.append(p_values[i][1] * p_values[i][1])
+median2 = 49/81
+
+gc = median / median2
+
+
+
+with open('./output.txt', 'w') as output_file:
+    output_file.write('<A>\n')
+    for x in p_values:
+        output_file.write(str(x[0]) + ':' + str(x[1]) + '\n')
+    output_file.write('</A>'+ '\n')
+    output_file.write('<B>'+ '\n')
+    for x in p_values_significant:
+        output_file.write(str(x[0]) + ':' + str(x[1])+ '\n')
+    output_file.write('</B>'+ '\n')
+    output_file.write('</C>'+ '\n')
+    output_file.write('Lambda_gc:' + str(gc))
+    output_file.write('</C>'+ '\n')
+
 
